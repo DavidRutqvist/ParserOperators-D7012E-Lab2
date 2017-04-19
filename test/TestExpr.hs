f661fc73b3f013184e3827669d4fdea1eefc4e8a
David Rutqvist <david@rutqvist.com>
2017-04-19 13:14:45 +0200

    value in Expr implemented successfully and satisfying all tests (also added some more tests since the provided tests in the lab specification were a bit too insufficient)

--- test/TestExpr.hs
+++ test/TestExpr.hs
@@ -10,6 +10,8 @@ test = do
         quickCheck n2
         quickCheck n3
         quickCheck n4
+        quickCheck n5
+        quickCheck n6
         quickCheck $ expectFailure $ seq (n21) True
         quickCheck $ expectFailure $ seq (n31) True
 
@@ -25,5 +27,7 @@ n1 = (testValue "1") == 1
 n2 = (testValue "x") == 1
 n3 = (testValue "x+y") == 3
 n4 = (testValue "x-y-y") == -3
+n5 = (testValue "2/2") == 1
+n6 = (testValue "2*3") == 6
 n21 = testValue "1/(2-y)" == 1 {-  Expr.value: division by 0 -}
 n31 = testValue "2+z" == 2   {-  Expr.value: undefined variable z -}
