cc00fbc2bd8c2faf9fba3d5eaf728dbdc896864d
David Rutqvist <david@rutqvist.com>
2017-04-20 20:07:25 +0200

    Implemented statement constructors and parsing for individual statement

--- src/Statement.hs
+++ src/Statement.hs
@@ -1,20 +1,47 @@
-module Statement(T, parse, toString, fromString, exec) where
-import Prelude hiding (return, fail)
-import Parser hiding (T)
+module Statement(T, parse, toString, fromString, exec, block) where
 import qualified Dictionary
 import qualified Expr
+import           Parser     hiding (T)
+import           Prelude    hiding (fail, return)
 type T = Statement
 data Statement =
-    Assignment String Expr.T |
-    If Expr.T Statement Statement
+        Assignment String Expr.T
+      | If Expr.T Statement Statement
+      | Skip
+      | While Expr.T Statement
+      | Read String
+      | Write Expr.T
+      | Block [Statement]--Begin {Statements} End
+      | Repeat Statement Expr.T
     deriving Show
 
 assignment = word #- accept ":=" # Expr.parse #- require ";" >-> buildAss
 buildAss (v, e) = Assignment v e
 
+if' = accept "if" -# Expr.parse #- require "then" # parse #- require "else" # parse >-> buildIf
+buildIf ((expression, thenStatements), elseStatements) = If expression thenStatements elseStatements
+
+skip = accept "skip" #- require ";" >-> buildSkip
+buildSkip (_) = Skip
+
+while = accept "while" -# Expr.parse #- require "do" # parse >-> buildWhile
+buildWhile (e, s) = While e s
+
+read' = accept "read" -# word #- require ";" >-> buildRead
+buildRead (v) = Read v
+
+write = accept "write" -# Expr.parse #- require ";" >-> buildWrite
+buildWrite (e) = Write e
+
+block = accept "begin" -# iter parse #- require "end" >-> buildBlock
+buildBlock (statements) = Block statements
+
+doWhile = accept "repeat" -# parse #- require "until" # Expr.parse #- require ";" >-> buildDoWhile
+buildDoWhile (s, e) = Repeat s e
+
 exec :: [T] -> Dictionary.T String Integer -> [Integer] -> [Integer]
-exec (If cond thenStmts elseStmts: stmts) dict input = 
-    if (Expr.value cond dict)>0 
+exec (If cond thenStmts elseStmts: stmts) dict input =
+    if (Expr.value cond dict)>0
     then exec (thenStmts: stmts) dict input
     else exec (elseStmts: stmts) dict input
 
